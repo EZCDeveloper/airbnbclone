49f1f799bc5f66ba77f4180abf65083cf0422bc1
Manu Zini <emanuel.zini@gmail.com>
2022-01-24 07:43:07 -0300

    day 4

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
   images:{
     domains: ['emanuelzini.com.ar', 'links.papareact.com'],
   },
+  env: {
+    mapbox_key: 'pk.eyJ1Ijoic2FndW50byIsImEiOiJjazZnZjVzZjQyajN5M25ueml1bXYwMXNnIn0.yEiJ60gcTRHuE2fx3M7TsQ'
+  }
 };
